5688eebf9ca612b1ed21c8241d89d43e3b79a072
Samuel da Silva <samosaara@gmail.com>
2022-11-07 14:57:29 +0100

    feat: more python doom configs

--- dot_doom.d/config.el
+++ dot_doom.d/config.el
@@ -6,8 +6,8 @@
 
 ;; Some functionality uses this to identify you, e.g. GPG configuration, email
 ;; clients, file templates and snippets. It is optional.
-(setq user-full-name "John Doe"
-      user-mail-address "john@doe.com")
+(setq user-full-name "Samuel da Silva"
+      user-mail-address "samosaara@gmail.com")
 
 ;; Doom exposes five (optional) variables for controlling fonts in Doom:
 ;;
@@ -49,6 +49,12 @@
 ;;   (after! PACKAGE
 ;;     (setq x y))
 ;;
+(after! projectile
+  (push "pyproject.toml" projectile-project-root-files-bottom-up))
+
+(after! dap-mode
+  (setq dap-python-debugger 'debugpy))
+
 ;; The exceptions to this rule:
 ;;
 ;;   - Setting file/directory variables (like `org-directory')
